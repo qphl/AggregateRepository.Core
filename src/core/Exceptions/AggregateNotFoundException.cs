2cf95881a60944b163cfb412b2ed158c4fe082d4
Rich Knight <richard.knight@bathasu.com>
2013-05-10 13:40:37 +0100

    exceptions added

---
+++ src/core/Exceptions/AggregateNotFoundException.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CR.AggregateRepository.Core.Exceptions
+{
+    [Serializable]
+    public class AggregateNotFoundException : Exception
+    {
+        public AggregateNotFoundException()
+        {
+        }
+
+        public AggregateNotFoundException(string message) : base(message)
+        {
+        }
+
+        public AggregateNotFoundException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected AggregateNotFoundException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+    [Serializable]
+    public class AggregateVersionException : Exception
+    {
+
+        public AggregateVersionException()
+        {
+        }
+
+        public AggregateVersionException(string message) : base(message)
+        {
+        }
+
+        public AggregateVersionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected AggregateVersionException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+
+}
---
+++ src/core/Exceptions/AggregateVersionException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace CR.AggregateRepository.Core.Exceptions
+{
+    [Serializable]
+    public class AggregateVersionException : Exception
+    {
+
+        public AggregateVersionException()
+        {
+        }
+
+        public AggregateVersionException(string message) : base(message)
+        {
+        }
+
+        public AggregateVersionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected AggregateVersionException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
