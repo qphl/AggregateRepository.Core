030da4c63c495008a01db82905976e78f27c1b93
Rich Knight <richard.knight@bathasu.com>
2013-05-10 14:06:50 +0100

    added test project and in memory impl

---
+++ src/persistance.memory/InMemoryAggregateRepository.cs
@@ -0,0 +1,17 @@
+﻿using CR.AggregateRepository.Core;
+
+namespace CR.AggregateRepository.Persistance.Memory
+{
+    public class InMemoryAggregateRepository : IAggregateRepository
+    {
+        public void Save(IAggregate aggregateToSave, int expectedAggregateVersion)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T GetAggregateFromRepository<T>(string aggregateId) where T : IAggregate
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/tests/AggregateRepositoryTestFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CR.AggregateRepository.Core;
+
+namespace tests
+{
+    public abstract class AggregateRepositoryTestFixture
+    {
+
+        protected IAggregateRepository Repository { get; set; }
+
+
+    }
+}
