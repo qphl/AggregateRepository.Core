23df4e147ae6ce12506e97089a6860aa6121cfe4
Luke Parker <moongateuk@gmail.com>
2018-05-01 11:39:06 +0100

    Code style fixes for Exceptions

--- src/Core/Exceptions/AggregateNotFoundException.cs
+++ src/Core/Exceptions/AggregateNotFoundException.cs
@@ -1,29 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
+﻿// <copyright file="AggregateNotFoundException.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
 
 namespace CR.AggregateRepository.Core.Exceptions
 {
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Thrown in the event of an aggregate not being found.
+    /// </summary>
     [Serializable]
     public class AggregateNotFoundException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateNotFoundException"/> class.
+        /// </summary>
         public AggregateNotFoundException()
         {
         }
 
-        public AggregateNotFoundException(string message) : base(message)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">Message displayed with the exception.</param>
+        public AggregateNotFoundException(string message)
+            : base(message)
         {
         }
 
-        public AggregateNotFoundException(string message, Exception inner) : base(message, inner)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateNotFoundException"/> class.
+        /// </summary>
+        /// <param name="message">Message displayed with the exception.</param>
+        /// <param name="inner">The exception that threw this exception.</param>
+        public AggregateNotFoundException(string message, Exception inner)
+            : base(message, inner)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateNotFoundException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">COntext</param>
         protected AggregateNotFoundException(
             SerializationInfo info,
-            StreamingContext context) : base(info, context)
+            StreamingContext context)
+            : base(info, context)
         {
         }
     }
--- src/Core/Exceptions/AggregateVersionException.cs
+++ src/Core/Exceptions/AggregateVersionException.cs
@@ -1,27 +1,53 @@
-﻿using System;
-using System.Runtime.Serialization;
+﻿// <copyright file="AggregateVersionException.cs" company="Cognisant">
+// Copyright (c) Cognisant. All rights reserved.
+// </copyright>
 
 namespace CR.AggregateRepository.Core.Exceptions
 {
+    using System;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Thrown when an incorrect aggregate version is specified
+    /// </summary>
     [Serializable]
     public class AggregateVersionException : Exception
     {
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateVersionException"/> class
+        /// </summary>
         public AggregateVersionException()
         {
         }
 
-        public AggregateVersionException(string message) : base(message)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateVersionException"/> class.
+        /// </summary>
+        /// <param name="message">Message displayed with the exception.</param>
+        public AggregateVersionException(string message)
+            : base(message)
         {
         }
 
-        public AggregateVersionException(string message, Exception inner) : base(message, inner)
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateVersionException"/> class.
+        /// </summary>
+        /// <param name="message">Message displayed with the exception</param>
+        /// <param name="inner">The exception that threw this exception</param>
+        public AggregateVersionException(string message, Exception inner)
+            : base(message, inner)
         {
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AggregateVersionException"/> class.
+        /// </summary>
+        /// <param name="info">Serialization info</param>
+        /// <param name="context">Context</param>
         protected AggregateVersionException(
             SerializationInfo info,
-            StreamingContext context) : base(info, context)
+            StreamingContext context)
+            : base(info, context)
         {
         }
     }
