6d28c57196844f91b65561b71dcffc1c4a511b75
Isaac Beizsley <23095702+iBeizsley@users.noreply.github.com>
2018-06-20 15:29:38 +0100

    Fix Build.sh testing

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ if [ -n "$2" ]; then tag="$2"
 fi
 tag=${tag/tags\//}
 
-dotnet test .\src\AggregateRepository.Core.Tests\AggregateRepository.Core.Tests.csproj
+dotnet test .\\src\\AggregateRepository.Core.Tests\\AggregateRepository.Core.Tests.csproj
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 dotnet pack .\\src\\core\\core.csproj -o ..\\..\\dist -p:Version="$version" -p:PackageVersion="$version" -p:Tag="$tag" -c Release
