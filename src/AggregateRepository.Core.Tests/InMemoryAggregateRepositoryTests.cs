198e254bfea85b8e7188cd56109aed8b75643bfd
Rich Knight <548432+rcknight@users.noreply.github.com>
2023-05-31 15:45:32 +0100

    Implement Async AggregateRepository interface

--- src/AggregateRepository.Core.Tests/InMemoryAggregateRepositoryTests.cs
+++ src/AggregateRepository.Core.Tests/InMemoryAggregateRepositoryTests.cs
@@ -2,16 +2,23 @@
 // Copyright (c) Corsham Science. All rights reserved.
 // </copyright>
 
+using System.Threading.Tasks;
+
 namespace AggregateRepository.Core.Tests
 {
     using CorshamScience.AggregateRepository.Core;
 
     public class InMemoryAggregateRepositoryTests : AggregateRepositoryTestFixture
     {
-        protected override void InitRepository() => RepoUnderTest = new InMemoryAggregateRepository();
-
-        protected override void CleanUpRepository()
+        protected override Task InitRepository()
         {
+            RepoUnderTest = new InMemoryAggregateRepository();
+            return Task.CompletedTask;
+        }
+
+        protected override Task CleanUpRepository()
+        {
+            return Task.CompletedTask;
         }
     }
 }
